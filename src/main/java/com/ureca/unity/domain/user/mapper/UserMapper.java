33a56fe58f0450160f48dbd2a19d5443c52699a8
joonhyong <dbshwns0528@naver.com>
2026-01-08 16:16:29 +0900

    chore: UserMapper 변환타입 Optioanl로 변경

--- src/main/java/com/ureca/unity/domain/user/mapper/UserMapper.java
+++ src/main/java/com/ureca/unity/domain/user/mapper/UserMapper.java
@@ -3,11 +3,12 @@ package com.ureca.unity.domain.user.mapper;
 import com.ureca.unity.domain.user.model.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
 
-    User findByProviderAndProviderId(
+    Optional<User> findByProviderAndProviderId(
             @Param("provider") String provider,
             @Param("providerId") String providerId
     );
