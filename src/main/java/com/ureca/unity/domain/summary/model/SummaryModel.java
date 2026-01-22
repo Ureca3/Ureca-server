0a34935a3b226d95c3b2f0fe8fab473e6dc516e2
Zoo2-bi <bagjui068@gmail.com>
2026-01-22 11:15:11 +0900

    fix: 오타 수정

--- src/main/java/com/ureca/unity/domain/summary/model/SummaryModel.java
+++ src/main/java/com/ureca/unity/domain/summary/model/SummaryModel.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Getter
 public class SummaryModel {
     private Long summaryId;
-    private Long sttJodId;
+    private Long sttJobId;
     private Long counselingId;
     private Long userId;
 
