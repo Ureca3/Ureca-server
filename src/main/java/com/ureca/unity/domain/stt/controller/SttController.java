64baac69e670eb4b0b0bb17cade86220188c92ea
joonhyong <dbshwns0528@naver.com>
2026-01-24 17:32:00 +0900

    chore: 스웨거 Authorization 추가 및 api 도메인별로 ë분류

--- src/main/java/com/ureca/unity/domain/stt/controller/SttController.java
+++ src/main/java/com/ureca/unity/domain/stt/controller/SttController.java
@@ -2,9 +2,14 @@ package com.ureca.unity.domain.stt.controller;
 
 import com.ureca.unity.domain.stt.model.SttJob;
 import com.ureca.unity.domain.stt.service.SttService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "3. STT",
+        description = "음성 인식(STT) API"
+)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/counselings")
